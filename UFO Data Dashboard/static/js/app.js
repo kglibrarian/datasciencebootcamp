53fa6ed6704cd74ecaaae11a6b570937b30d9340
kglibrarian <karen.gutzman@northwestern.edu>
2019-06-01 17:36:21 -0500

    finished homework

--- UFO Data Dashboard/static/js/app.js
+++ UFO Data Dashboard/static/js/app.js
@@ -1,40 +1,63 @@
 // from data.js
 var tableData = data;
-//console.log(Object.keys(tableData[0]))
-// YOUR CODE HERE!
-var table = d3.select("#ufo-table-2").append("table");
-thead = table.append("thead").append("tr");
-tbody = table.append("tbody");
 
-var i;
-ufo_array = []
-for (i = 0; i < tableData.length; i++) {
-    text = Object.values(tableData[i])
-    //console.log(text)
-    ufo_array.push(text);
-    //console.log(ufo_array)
-    //date = text[0]
-    //d3.selectAll("th").append("tr").text(date)
-    //console.log(tableData[i])
-    //console.log(Object.values(tableData[i]))
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-   // First create the table rows 
-   var tr = tbody.selectAll("tr")  
-        .data(ufo_array, function(d,i){
-         console.log(d)
-         return d;  
-     })
-        .enter()
-        .append("tr");
-// Now create the table cells   
-    var td = tr.selectAll("td") 
-        .data(function(d) {return d; })
-        .enter()
-        .append("td")
-        .text(function(d) {return d;});
-    ufo_array = []
+ // Loop Through `table data` and console.log each report object
+data.forEach(function(tableData) {
+    //console.log(tableData);
+});
 
-    //http://jsfiddle.net/W7Zwg/1/
-} 
+// Use d3 to update each cell's text with report values 
+data.forEach(function(tableData) {
+    //console.log(tableData);
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(function([key, value]) {
+    //console.log(key, value);
+//Append a cell to the row for each value in thereport object
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
 
- 
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  //console.log(inputValue);
+  //console.log(tableData);
+  var filteredData = tableData.filter(date => date.datetime === inputValue);
+  console.log(filteredData);
+  
+  //clear the table of data
+  d3.selectAll("tbody td").remove();
+
+  //refill the table with filteredData
+  data.forEach(function(filteredData) {
+    //console.log(filteredData);
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(function([key, value]) {
+        //console.log(key, value);
+        //Append a cell to the row for each value in thereport object
+        var cell = row.append("td");
+        cell.text(value);
+        });
+
+
+    });
+    document.getElementById('datetime').value = ''
+
+});
+
+
